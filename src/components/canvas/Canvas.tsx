20d31c0ac02ebf80c543de97e605811361a5d31f
James Lester <james@jameslester.xyz>
2020-08-23 18:09:34 +0100

    Added canvas and menu components

---
+++ src/components/canvas/Canvas.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import style from "./Canvas.module.scss";
+
+type CanvasProps = {};
+
+const Canvas: React.FC<CanvasProps> = (props) => {
+  return <div></div>;
+};
+
+export default Canvas;
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import style from "./Menu.module.scss";
+
+type MenuProps = {};
+
+const Menu: React.FC<MenuProps> = (props: MenuProps) => {
+  return <div></div>;
+};
+
+export default Menu;
